079611f27446c2e8b4a29e25962d5177e4babccb
Ziad Ahmed <zisakh@gmail.com>
2024-05-03 02:50:56 +0300

    change link backend

--- src/requests/axios.js
+++ src/requests/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://pegasus-fun-locust.ngrok-free.app/",
 });
 
 instance.interceptors.request.use(
